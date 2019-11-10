1b5e2644ea07ffcbce78ea63dbc8bf7b8335b1ad
Stefano Righini <stefano.righini6@studio.unibo.it>
2019-11-11 00:16:18 +0100

    Add gradle configurations to run scala tests.

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,10 +9,24 @@ repositories {
 
 dependencies {
     implementation(group = "org.scala-lang", name = "scala-library", version = "2.12.2")
-    implementation (group = "com.typesafe.akka", name = "akka-actor_2.12", version = "2.6.0")
-    implementation (group = "com.typesafe.play", name = "play-json_2.12", version = "2.7.3")
-    
+    implementation(group = "com.typesafe.akka", name = "akka-actor_2.12", version = "2.6.0")
+    implementation(group = "com.typesafe.play", name = "play-json_2.12", version = "2.7.3")
+
     testImplementation(group = "org.scalatest", name = "scalatest_2.12", version = "3.0.8")
     testImplementation(group = "com.typesafe.akka", name = "akka-testkit_2.12", version = "2.6.0")
     testImplementation(group = "com.typesafe.akka", name = "akka-actor-testkit-typed_2.12", version = "2.6.0")
 }
+
+tasks.withType<ScalaCompile> {
+    sourceCompatibility = "1.8"
+    targetCompatibility = "1.8"
+}
+
+task<JavaExec>("scalaTest") {
+    dependsOn("testClasses")
+    main = "org.scalatest.tools.Runner"
+    args(listOf("-R", "build/classes/scala/test", "-o"))
+    classpath = sourceSets.test.get().runtimeClasspath
+}
+
+tasks.test.get().dependsOn("scalaTest")
