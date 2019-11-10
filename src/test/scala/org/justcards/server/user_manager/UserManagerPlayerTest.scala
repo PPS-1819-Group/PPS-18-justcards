4bcec2dbede8349ce0e5c835f5ecc50ce42d0070
Francesco Grandinetti <francesc.grandinetti@studio.unibo.it>
2019-11-10 18:45:09 +0100

    refactor UserManagerTest

--- src/test/scala/org/justcards/server/user_manager/UserManagerPlayerTest.scala
+++ src/test/scala/org/justcards/server/user_manager/UserManagerPlayerTest.scala
@@ -1,24 +1,31 @@
 package org.justcards.server.user_manager
 
-import scala.concurrent.duration._
-import akka.actor.{Actor, ActorRef, ActorSystem, Props}
-import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
+import akka.actor.{Actor, ActorRef, ActorSystem, PoisonPill, Props}
+import org.scalatest.{BeforeAndAfter, BeforeAndAfterAll, Matchers, WordSpecLike}
 import akka.testkit.{ImplicitSender, TestKit}
 import org.justcards.commons._
-import org.justcards.server.user_manager.UserManagerMessage.{Players, RetrieveAllPlayers, UserInfo, UserLogout}
+import org.justcards.server.user_manager.UserManagerMessage._
 
-class UserManagerTest extends TestKit(ActorSystem("ActorTest")) with ImplicitSender with WordSpecLike
-  with Matchers with BeforeAndAfterAll {
+class UserManagerPlayerTest extends TestKit(ActorSystem("UserManagerPlayerTest")) with ImplicitSender with WordSpecLike
+  with Matchers with BeforeAndAfterAll with BeforeAndAfter {
 
-  import UserManagerTest._
+  import UserManagerPlayerTest._
+
+  var userManager: ActorRef = _
+
+  before {
+    userManager = system.actorOf(UserManager(null))
+  }
+
+  after {
+    userManager ! PoisonPill
+  }
 
   override def afterAll: Unit = {
     TestKit.shutdownActorSystem(system)
   }
 
   "The user manager" when {
-
-    val userManager = system.actorOf(UserManager())
 
     "created" should {
 
@@ -36,22 +43,24 @@ class UserManagerTest extends TestKit(ActorSystem("ActorTest")) with ImplicitSen
       }
 
       "save the user after its registration" in {
+        doLogIn(userManager, TEST_USERNAME)
         expectUsers(userManager, UserInfo(TEST_USERNAME, testActor))
       }
 
       "not register a user if the username is already present" in {
+        doLogIn(userManager, TEST_USERNAME)
         userManager ! LogIn(TEST_USERNAME)
         expectMsgType[ErrorOccurred]
       }
 
       "not allow to the same user to register twice with different username" in {
-        userManager ! LogIn(DOUBLE_USER_USERNAME)
-        receiveN(1)
+        doLogIn(userManager, DOUBLE_USER_USERNAME)
         userManager ! LogIn(DOUBLE_USER_USERNAME + "-retry")
         expectMsgType[ErrorOccurred]
       }
 
       "unregister a user" in {
+        doLogIn(userManager, TEST_USERNAME)
         userManager ! UserLogout(TEST_USERNAME, testActor)
         expectNoMessage()
         expectNoUsers(userManager)
@@ -73,20 +82,25 @@ class UserManagerTest extends TestKit(ActorSystem("ActorTest")) with ImplicitSen
 
   }
 
+  private def doLogIn(userManager: ActorRef, username: String): Unit = {
+    userManager ! LogIn(username)
+    receiveN(1)
+  }
+
   private def expectUsers(userManager: ActorRef, users: UserInfo*): Unit = {
     expectUsers(userManager, users.toSet)
   }
 
   private def expectUsers(userManager: ActorRef, users: Set[UserInfo]): Unit = {
     userManager ! RetrieveAllPlayers(testActor)
-    expectMsg(Players(users.toSet))
+    expectMsg(Players(users))
   }
 
   private def expectNoUsers(userManager: ActorRef): Unit = expectUsers(userManager)
 
 }
 
-object UserManagerTest {
+object UserManagerPlayerTest {
 
   val TEST_USERNAME: String = "test-username"
   val MULTI_TEST_USERNAME: String = "multi-test-username"
