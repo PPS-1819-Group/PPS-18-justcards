f51a15f22ca231d186cd6df50f54a2518b07bba1
Francesco Grandinetti <francesc.grandinetti@studio.unibo.it>
2019-11-13 19:19:30 +0100

    refactor PlayerManager abstracting from the user base of information

--- src/main/scala/org/justcards/server/user_manager/PlayerManager.scala
+++ src/main/scala/org/justcards/server/user_manager/PlayerManager.scala
@@ -4,13 +4,13 @@ import akka.actor.{Actor, ActorRef, Props}
 import org.justcards.commons.{ErrorOccurred, Logged}
 import org.justcards.server.user_manager.UserManagerMessage._
 
-private[user_manager] class PlayerManager extends Actor {
+private[user_manager] class PlayerManager(playerDatabase: PlayerDatabase) extends Actor {
 
   import org.justcards.commons.AppError._
 
-  override def receive: Receive = defaultBehaviour()
+  override def receive: Receive = defaultBehaviour(playerDatabase)
 
-  private def defaultBehaviour(users: Set[(String, ActorRef)] = Set()): Receive = {
+  private def defaultBehaviour(users: PlayerDatabase): Receive = {
     case UserLogIn(message, user) =>
       val loggedUser = users find(userData =>
         userData._2 == user || userData._1 == message.username
@@ -26,7 +26,7 @@ private[user_manager] class PlayerManager extends Actor {
       }
     case UserLogout(username, user) =>
       if (users contains (username -> user))
-        context become defaultBehaviour(users - (username -> user))
+        context become defaultBehaviour(users - username)
     case RetrieveAllPlayers(msgSender) =>
       val usersSet: Set[UserInfo] = users map(data => UserInfo(data._1, data._2))
       msgSender ! Players(usersSet)
@@ -38,5 +38,35 @@ private[user_manager] class PlayerManager extends Actor {
 }
 
 private[user_manager] object PlayerManager {
-  def apply(): Props = Props(classOf[PlayerManager])
+  def apply(): Props = Props(classOf[PlayerManagerWithSet])
+  private[this] class PlayerManagerWithSet extends PlayerManager(PlayerDatabase.createSetPlayerDatabase())
 }
+
+/**
+  * Trait for classes that should realise the players database
+  */
+trait PlayerDatabase extends Set[(String, ActorRef)] {
+  def +(value: (String, ActorRef)): PlayerDatabase
+  def -(elem: (String, ActorRef)): PlayerDatabase
+  def -(value: String): PlayerDatabase
+}
+
+object PlayerDatabase {
+
+  def createSetPlayerDatabase(): PlayerDatabase = PlayerDbSet()
+
+  private[this] case class PlayerDbSet(set: Set[(String, ActorRef)] = Set()) extends PlayerDatabase {
+
+    override def +(value: (String, ActorRef)): PlayerDatabase = PlayerDbSet(set + value)
+
+    override def -(username: String): PlayerDatabase = PlayerDbSet(set filterNot(_._1 == username))
+
+    override def contains(elem: (String, ActorRef)): Boolean = set contains elem
+
+    override def -(elem: (String, ActorRef)): PlayerDatabase = PlayerDbSet(set - elem)
+
+    override def iterator: Iterator[(String, ActorRef)] = set.iterator
+  }
+
+}
+
