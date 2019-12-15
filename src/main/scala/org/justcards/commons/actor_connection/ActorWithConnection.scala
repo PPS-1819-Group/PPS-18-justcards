b30e68c1fd9aee691a8bff9453e84a450585127d
Francesco Grandinetti <francesc.grandinetti@studio.unibo.it>
2019-12-15 15:46:02 +0100

    improve ActorWithConnection

--- src/main/scala/org/justcards/commons/actor_connection/ActorWithConnection.scala
+++ src/main/scala/org/justcards/commons/actor_connection/ActorWithConnection.scala
@@ -4,8 +4,14 @@ import akka.actor.{Actor, ActorRef}
 import org.justcards.commons.AppMessage
 
 abstract class ActorWithConnection extends Actor {
-  def parse: Receive
-  final def become(behaviour: Receive): Unit = context become (parse orElse behaviour)
+
+  protected def parse: Receive
+  protected def init: Receive
+
+  final override def receive: Receive = parse orElse init
+  final def become(behaviour: Receive, discardOld: Boolean = true): Unit =
+    context become (parse orElse behaviour, discardOld)
+
   private[actor_connection] def tellWithConnection(actor: ActorRef, message: AppMessage): Unit
   implicit class MyPersonalSender(actor: ActorRef) {
     def ==>(message: AppMessage): Unit = tellWithConnection(actor, message)
